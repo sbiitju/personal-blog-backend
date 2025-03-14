95d8db22eedde391f9ec01720fc7fe1f2e4dbc31
Hosain <cryptichasan925@gmail.com>
2025-03-14 16:03:02 +0600

    pre password hashed method

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
 import { model, Schema } from 'mongoose';
 import { IUser, UserModel } from './user.interface';
+import bcrypt from 'bcrypt';
+import config from '../../config';
 
 const userSchema = new Schema<IUser, UserModel>(
   {
@@ -32,5 +34,18 @@ const userSchema = new Schema<IUser, UserModel>(
   },
   { timestamps: true },
 );
+
+
+userSchema.pre('save', async function (next) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this
+    user.password = await bcrypt.hash(
+      user.password,
+      Number(config.bcrypt_salt_rounds),
+    )
+    next()
+  })
+
+
 
 export const User = model<IUser, UserModel>('User', userSchema);
