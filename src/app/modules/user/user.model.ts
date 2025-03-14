217fc76c9652221c261e2d9064ef3677be94b0a4
Hosain <cryptichasan925@gmail.com>
2025-03-14 16:00:35 +0600

    user model setup

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,7 +1,7 @@
-import { Schema } from 'mongoose';
-import { IUser } from './user.interface';
+import { model, Schema } from 'mongoose';
+import { IUser, UserModel } from './user.interface';
 
-const userSchema = new Schema<IUser>(
+const userSchema = new Schema<IUser, UserModel>(
   {
     password: {
       type: String,
@@ -33,3 +33,4 @@ const userSchema = new Schema<IUser>(
   { timestamps: true },
 );
 
+export const User = model<IUser, UserModel>('User', userSchema);
