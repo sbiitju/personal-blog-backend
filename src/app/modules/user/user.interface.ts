bb46cd901e6ddb80a8569a86a4bb122d659a3fad
Hosain <cryptichasan925@gmail.com>
2025-03-14 15:53:06 +0600

    user role

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,8 +1,12 @@
+import { USER_ROLE } from './user.constance';
+
 export interface IUser {
   password: string;
   needsPasswordChange: boolean;
-  role: string;
+  role: 'admin' | 'user';
   type: string;
   isDeleted: boolean;
   isBlocked: boolean;
 }
+
+export type TUserRole = keyof typeof USER_ROLE;
