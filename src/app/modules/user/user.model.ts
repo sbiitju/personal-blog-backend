fc3e607fe142395a0d5fc0560a53ab64f60f3b26
Hosain <cryptichasan925@gmail.com>
2025-03-14 16:11:57 +0600

    jwt issue showing

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -72,4 +72,13 @@ userSchema.statics.isUserPasswordMatch = async function (
   return await bcrypt.compare(plainTextPassword, hashedPassword);
 };
 
+userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+  passwordChangedTimestamp: Date,
+  jwtIssuedTimestamp: number,
+) {
+  const passwordChangedTime =
+    new Date(passwordChangedTimestamp).getTime() / 1000;
+  return passwordChangedTime > jwtIssuedTimestamp;
+};
+
 export const User = model<IUser, UserModel>('User', userSchema);
