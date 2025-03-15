a601d78a5a71b93af53594a2a77e4bb23623a09b
Hosain <cryptichasan925@gmail.com>
2025-03-15 14:28:21 +0600

    content interface done

---
+++ src/app/modules/content/content.interface.ts
@@ -0,0 +1,10 @@
+export interface IContent {
+  doomain: string;
+  title: string;
+  date: Date;
+  place: string;
+  description: string;
+  photo: string;
+  category: string;
+  subcategory: string;
+}
